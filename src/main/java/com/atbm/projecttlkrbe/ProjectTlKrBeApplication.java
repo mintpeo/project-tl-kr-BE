377d32cac1fb9966610edea5b542f068c109b558
mintpeo <tranquocminh15981@gmail.com>
2026-07-30 20:25:05 +0700

    First Commit

---
+++ src/main/java/com/atbm/projecttlkrbe/ProjectTlKrBeApplication.java
@@ -0,0 +1,13 @@
+package com.atbm.projecttlkrbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTlKrBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTlKrBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atbm/projecttlkrbe/ProjectTlKrBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atbm.projecttlkrbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTlKrBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
